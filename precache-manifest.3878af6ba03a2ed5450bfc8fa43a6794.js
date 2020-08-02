d0fd7003221e81ac8399142ea76ca9d1e7807336
Yurii <yurii.kryvko@gmail.com>
2020-08-02 14:15:30 +0300

    Updates

---
+++ precache-manifest.3878af6ba03a2ed5450bfc8fa43a6794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f8eaf9d029f87b8e01e7d95ebe5fc1",
+    "url": "/My-Twitter/index.html"
+  },
+  {
+    "revision": "59be901329ef4ce7ea51",
+    "url": "/My-Twitter/static/css/main.7502589f.chunk.css"
+  },
+  {
+    "revision": "c542aa2cb13a707d37f6",
+    "url": "/My-Twitter/static/js/2.a047e2a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Twitter/static/js/2.a047e2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59be901329ef4ce7ea51",
+    "url": "/My-Twitter/static/js/main.2b50f31e.chunk.js"
+  },
+  {
+    "revision": "4b68c8e5b72bc07bc16e",
+    "url": "/My-Twitter/static/js/runtime-main.137d9fa3.js"
+  }
+]);
