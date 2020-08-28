08653f5c82b4cc433c37be0e988560a15881f587
Yurii <yurii.kryvko@gmail.com>
2020-08-28 16:20:16 +0300

    Updates

---
+++ precache-manifest.14ec8f05c6c6bfb5c4eb168c71291d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93337cf4821440ff5ac712d62977ec2f",
+    "url": "/My-Twitter-React-app/index.html"
+  },
+  {
+    "revision": "b12aafd68d03b1114a97",
+    "url": "/My-Twitter-React-app/static/css/main.2fcd0274.chunk.css"
+  },
+  {
+    "revision": "c542aa2cb13a707d37f6",
+    "url": "/My-Twitter-React-app/static/js/2.a047e2a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Twitter-React-app/static/js/2.a047e2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12aafd68d03b1114a97",
+    "url": "/My-Twitter-React-app/static/js/main.6e908215.chunk.js"
+  },
+  {
+    "revision": "99503e3883fb95f21af9",
+    "url": "/My-Twitter-React-app/static/js/runtime-main.6364f7b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/My-Twitter/precache-manifest.9b5d4413e84ac3d1df58d35d42fb53a6.js"
+  "/My-Twitter-React-app/precache-manifest.14ec8f05c6c6bfb5c4eb168c71291d41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/My-Twitter/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/My-Twitter-React-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
